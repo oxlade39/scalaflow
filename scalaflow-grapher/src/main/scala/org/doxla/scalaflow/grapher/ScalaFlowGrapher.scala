f7e73788ab7cf3f0ad837a75a1b9d58c36f6abee
oxlade39 <dan.oxlade@gmail.com>
2010-03-31 23:13:16 +0100

    separated out the grapher project

--- scalaflow-grapher/src/main/scala/org/doxla/scalaflow/grapher/ScalaFlowGrapher.scala
+++ scalaflow-grapher/src/main/scala/org/doxla/scalaflow/grapher/ScalaFlowGrapher.scala
@@ -1,10 +1,11 @@
-package org.doxla.scalaflow.example
+package org.doxla.scalaflow.grapher
 
 import java.io.{PrintWriter, File}
 import org.doxla.scalaflow.{FlowEvent, FlowState, ScalaFlow}
 import com.google.inject.grapher.graphviz._
 import java.lang.String
 import java.util.Map
+import org.doxla.scalaflow.example.CustomerCoffeeWorkflow
 
 class ScalaFlowGrapher(renderer: GraphvizRenderer) {
   implicit def flowState2GraphvizNode(state: FlowState) = {
@@ -14,9 +15,9 @@ class ScalaFlowGrapher(renderer: GraphvizRenderer) {
     graphvizNode
   }
 
-//  implicit def flowEvent2GrapizEdge(event: FlowEvent) = {
-//    val edge = new GraphvizEdge(event.transitionsTo.)
-//  }
+  //  implicit def flowEvent2GrapizEdge(event: FlowEvent) = {
+  //    val edge = new GraphvizEdge(event.transitionsTo.)
+  //  }
 
   var flowToGraph: Option[ScalaFlow] = None
 
@@ -28,9 +29,9 @@ class ScalaFlowGrapher(renderer: GraphvizRenderer) {
   }
 
   def graph() {
-    for (state:FlowState <- flow.states) {
+    for (state: FlowState <- flow.states) {
       renderer.addNode(state)
-      for (event:FlowEvent <- state.events) {
+      for (event: FlowEvent <- state.events) {
         val edge = new LabeledGraphvizEdge(state.name.name, event.transitionsTo.state.name.name)
         edge.setStyle(EdgeStyle.SOLID)
         edge.setLabel(event.name.name)
@@ -42,19 +43,21 @@ class ScalaFlowGrapher(renderer: GraphvizRenderer) {
 
 }
 
-class LabeledGraphvizEdge(val tailId: String, val headId:String) extends GraphvizEdge(tailId, headId) {
+class LabeledGraphvizEdge(val tailId: String, val headId: String) extends GraphvizEdge(tailId, headId) {
   private[this] var label: Option[String] = None
 
   def getLabel = label.get
+
   def setLabel(label: String) = this.label = Some(label.replaceAll("_", " "))
+
   def hasLabel = label.isDefined
 }
 
 class CustomGraphvizRenderer extends GraphvizRenderer {
   override def getEdgeAttributes(edge: GraphvizEdge) = {
     val attributes: Map[String, String] = super.getEdgeAttributes(edge)
-    if( edge.isInstanceOf[LabeledGraphvizEdge] && edge.asInstanceOf[LabeledGraphvizEdge].hasLabel ) {
-      attributes.put("label", "<"+edge.asInstanceOf[LabeledGraphvizEdge].getLabel+">")
+    if (edge.isInstanceOf[LabeledGraphvizEdge] && edge.asInstanceOf[LabeledGraphvizEdge].hasLabel) {
+      attributes.put("label", "<" + edge.asInstanceOf[LabeledGraphvizEdge].getLabel + ">")
     }
     attributes
   }
@@ -63,7 +66,7 @@ class CustomGraphvizRenderer extends GraphvizRenderer {
 object Hello {
   def main(args: Array[String]) = {
     val renderer = new CustomGraphvizRenderer()
-//    val renderer = new GraphvizRenderer()
+    //    val renderer = new GraphvizRenderer()
     val printWriter = new PrintWriter(new File("testoutput.dot"), "UTF-8")
     renderer.setOut(printWriter)
     renderer.setRankdir("TB")
---
+++ scalaflow-grapher/src/test/scala/org/doxla/scalaflow/grapher/ScalaFlowGrapherTest.scala
@@ -0,0 +1,14 @@
+package org.doxla.scalaflow.grapher
+
+import org.specs.runner.JUnit4
+import org.junit.Test
+
+
+class ScalaFlowGrapherTest {
+
+  @Test
+  def testOutputProduced() {
+    Hello.main(null)
+  }
+
+}
