c3b4476aaeca686edf423ff94d24fb7a31bcdee4
oxlade39 <dan.oxlade@gmail.com>
2010-04-21 20:14:28 +0100

    adding accept non empty test

--- scalaflow-core/src/test/scala/org/doxla/scalaflow/engine/WorkflowInstanceSpecification.scala
+++ scalaflow-core/src/test/scala/org/doxla/scalaflow/engine/WorkflowInstanceSpecification.scala
@@ -4,14 +4,19 @@ import org.specs.runner.JUnit4
 import org.specs.Specification
 import org.doxla.scalaflow.{ScalaFlow, WorkflowSpecification}
 import org.doxla.scalaflow.exception.InvalidWorkflowException
+import org.doxla.scalaflow.example.CustomerCoffeeWorkflow
 
 
 class WorkflowInstanceSpecificationTest extends JUnit4(WorkflowInstanceSpecification)
 object WorkflowInstanceSpecification extends Specification {
 
   "A WorkflowInstance" should {
-    "not except empty Workflows" in {
+    "not except empty ScalaFlows" in {
       WorkflowInstance(emptyWorkflow) must throwA[InvalidWorkflowException]
+    }
+
+    "accept non empty ScalaFlows" in {
+      WorkflowInstance(CustomerCoffeeWorkflow) must notBeNull
     }
   }
 
