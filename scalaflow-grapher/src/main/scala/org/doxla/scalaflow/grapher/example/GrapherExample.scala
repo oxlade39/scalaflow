53ed8c2ed9242e7bb552a870a3b465d574046331
oxlade39 <dan.oxlade@gmail.com>
2010-05-04 22:59:16 +0100

    making the grapher a bit tiduier

--- scalaflow-grapher/src/main/scala/org/doxla/scalaflow/grapher/example/GrapherExample.scala
+++ scalaflow-grapher/src/main/scala/org/doxla/scalaflow/grapher/example/GrapherExample.scala
@@ -1,14 +1,14 @@
 package org.doxla.scalaflow.grapher.graphviz.example
 
 import org.doxla.scalaflow.example.CustomerCoffeeWorkflow
-import org.doxla.scalaflow.grapher.graphviz.ScalaGraphvizRenderer
+import org.doxla.scalaflow.grapher.graphviz.ScalaFlowGraphvizRenderer
 import java.io.{PrintWriter, File}
 import org.doxla.scalaflow.grapher.ScalaFlowGrapher
 
 
 object GrapherExample {
   def main(args: Array[String]) = {
-    val renderer = new ScalaGraphvizRenderer()
+    val renderer = new ScalaFlowGraphvizRenderer()
     val printWriter = new PrintWriter(new File("testoutput.dot"), "UTF-8")
     renderer.setOut(printWriter)
     renderer.setRankdir("TB")
--- scalaflow-grapher/src/main/scala/org/doxla/scalaflow/grapher/graphviz/ScalaFlowGraphvizRenderer.scala
+++ scalaflow-grapher/src/main/scala/org/doxla/scalaflow/grapher/graphviz/ScalaFlowGraphvizRenderer.scala
@@ -1,13 +1,19 @@
 package org.doxla.scalaflow.grapher.graphviz
 
 import com.google.inject.grapher.graphviz.{GraphvizRenderer, GraphvizEdge}
+import java.io.{PrintWriter, File}
 
-class ScalaGraphvizRenderer extends GraphvizRenderer {
+class ScalaFlowGraphvizRenderer extends GraphvizRenderer {
   override def getEdgeAttributes(edge: GraphvizEdge) = {
     val attributes = super.getEdgeAttributes(edge)
     if (edge.isInstanceOf[LabeledGraphvizEdge] && edge.asInstanceOf[LabeledGraphvizEdge].hasLabel) {
       attributes.put("label", "<" + edge.asInstanceOf[LabeledGraphvizEdge].getLabel + ">")
     }
     attributes
+  }
+
+  def >>(fileName: String) = {
+    setOut(new PrintWriter(new File(fileName), "UTF-8"))
+    setRankdir("TB")
   }
 }
