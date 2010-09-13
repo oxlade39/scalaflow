a068df2840d088abb15ab4d4120385824c833cea
oxlade39 <dan.oxlade@gmail.com>
2010-09-13 22:32:44 +0100

    upgraded to 2.8.0

--- project/build/ScalaFlowProject.scala
+++ project/build/ScalaFlowProject.scala
@@ -1,9 +1,8 @@
 import sbt._
 
-class ScalaFlowProject(info: ProjectInfo) extends ParentProject(info) with IdeaPlugin
+class ScalaFlowProject(info: ProjectInfo) extends ParentProject(info) //with IdeaPlugin
 {
-   val mavenLocal = "Local Maven Repository" at "file:///"+Path.userHome+"/.m2/repository"
-
+   val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
    // val junit = "junit" % "junit" % "4.4" % "test"
    // val specs = "org.scala-tools.testing" % "specs" % "1.6.0" % "test"
 
@@ -11,15 +10,16 @@ class ScalaFlowProject(info: ProjectInfo) extends ParentProject(info) with IdeaP
    lazy val ui = project("scalaflow-grapher", "ScalaFlow workflow visualisation", new ScalaFlowGrapherProject(_), core)
    
    
-   class ScalaFlowGrapherProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin
+   class ScalaFlowGrapherProject(info: ProjectInfo) extends DefaultProject(info) //with IdeaPlugin
    {
 	val guiceGrapher = "com.google.inject.extensions" % "guice-grapher" % "2.0"
    }
    
-   class ScalaFlowCoreProject(info: ProjectInfo) extends DefaultProject(info) with IdeaPlugin
+   class ScalaFlowCoreProject(info: ProjectInfo) extends DefaultProject(info) //with IdeaPlugin
    {
 	   val junit = "junit" % "junit" % "4.4" % "test"
-	    val specs = "org.scala-tools.testing" % "specs" % "1.6.0" % "test"
+	    // val specs = "org.scala-tools.testing" % "specs" % "1.6.0" % "test"
+	    val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"	
 //	   val specs = "org.scala-tools.testing" % "specs" % "1.6.6-2.8.0.Beta1-RC6" % "test"
    }
 
