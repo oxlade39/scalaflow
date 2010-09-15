fc458ad706db42b11b75236fdb415d4d66364af7
oxlade39 <dan.oxlade@gmail.com>
2010-09-15 20:10:19 +0100

    Fixing up StateBuilerSpec

--- project/build/ScalaFlowProject.scala
+++ project/build/ScalaFlowProject.scala
@@ -3,8 +3,6 @@ import sbt._
 class ScalaFlowProject(info: ProjectInfo) extends ParentProject(info) //with IdeaPlugin
 {
    val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
-   // val junit = "junit" % "junit" % "4.4" % "test"
-   // val specs = "org.scala-tools.testing" % "specs" % "1.6.0" % "test"
 
    lazy val core = project("scalaflow-core", "ScalaFlow Core", new ScalaFlowCoreProject(_))
    lazy val ui = project("scalaflow-grapher", "ScalaFlow workflow visualisation", new ScalaFlowGrapherProject(_), core)
@@ -12,15 +10,14 @@ class ScalaFlowProject(info: ProjectInfo) extends ParentProject(info) //with Ide
    
    class ScalaFlowGrapherProject(info: ProjectInfo) extends DefaultProject(info) //with IdeaPlugin
    {
-	val guiceGrapher = "com.google.inject.extensions" % "guice-grapher" % "2.0"
+		val guiceGrapher = "com.google.inject.extensions" % "guice-grapher" % "2.0"
    }
    
    class ScalaFlowCoreProject(info: ProjectInfo) extends DefaultProject(info) //with IdeaPlugin
    {
-	   val junit = "junit" % "junit" % "4.4" % "test"
-	    // val specs = "org.scala-tools.testing" % "specs" % "1.6.0" % "test"
-	    val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"	
-//	   val specs = "org.scala-tools.testing" % "specs" % "1.6.6-2.8.0.Beta1-RC6" % "test"
+		val junit = "junit" % "junit" % "4.7" % "test"
+	    val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
+		val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test"
    }
 
 }
--- scalaflow-core/src/test/scala/org/doxla/scalaflow/StateBuilderSpecificationTest.scala
+++ scalaflow-core/src/test/scala/org/doxla/scalaflow/StateBuilderSpecificationTest.scala
@@ -2,14 +2,19 @@ package org.doxla.scalaflow
 
 import component.{FlowState, FlowEvent}
 import org.specs.Specification
+import org.specs.specification.PendingUntilFixed
 import org.specs.runner.JUnit4
 import org.specs.mock.Mockito
+import org.mockito.Matchers
 
 class StateBuilderSpecificationTest extends JUnit4(StateBuilderSpecification)
 object StateBuilderSpecification extends Specification with Mockito {
 
-  object testStateBuilder extends StateBuilder {
-    val ctx = new Contextual[FlowEvent]
+  type ContextualFlowEventFunc = Function0[Contextual[FlowEvent]]
+  object ctx extends Contextual[FlowEvent]
+  val testCtx: Contextual[FlowEvent] = ctx
+
+  object testStateBuilder extends StateBuilder with SafeHelpers {    
     def event = ctx
     val stateRepository = mock[StateRepository]
   }
@@ -21,13 +26,11 @@ object StateBuilderSpecification extends Specification with Mockito {
       testStateBuilder.event.add( flowEvent )
       var setWhenStateCalled = false
 
-      val block = { setWhenStateCalled = true }
+      val block = { () => setWhenStateCalled = true }
 
       testStateBuilder.state('symbol_name){ block }
-      testStateBuilder.stateRepository.addState('symbol_name) {
-        block
-        flowEvent
-      } was called
+			
+      there was one(testStateBuilder.stateRepository).addState(Matchers.eq('symbol_name))( any[ContextualFlowEventFunc] )
     }
   }
 }
